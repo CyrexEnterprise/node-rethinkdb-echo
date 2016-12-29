1be289f7515f907ccf8be367311c0be591863178
Ricardo Malta <ricardo@cloudoki.com>
2016-12-29 18:12:23 +0000

    The real first commit
    Express boiler plate based on the express-generator
    Basic configuration setup
    HTTP setup
    Socket.io setup (backend+frontend)
    Message broker
    RethinkDB setup with Thinky, allowing the setup of a DB and a messages table
    
    Next step:
    Auto register user and set name
    Send messages
    Broadcast channel to simulate the PubSub

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var winston = require('winston');
+var expressWinston = require('express-winston');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./lib/controllers/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'lib/views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(expressWinston.logger({
+      transports: [
+        new winston.transports.Console({
+        //   json: true,
+          colorize: true
+        })
+      ],
+      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+      msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+      expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+      colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+      ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+    }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
