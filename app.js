eb7b7df8b420746dbf70b61f4671688929ec1d63
Ricardo Malta <ricardo@cloudoki.com>
2016-12-30 17:30:49 +0000

    Implemented
    Basic interface for chat (Bootstrap and JQuery)

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/messages', messages);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,43 @@
 function App(url){
 
-    const app = io.connect(url);
+    const socket = io.connect(url);
 
-    app.on('messages', function(message){
+    socket.on('messages', function(message) {
         console.log("message:", message);
     });
 
-    return app;
+    const sendMessage = function(message){
+        console.log("new message:", nickname, message);
+    }
+
+    const checkNickname = function() {
+
+        window.nickname = atob( $.cookie("echoblaster-nickname") || "" );
+        if( nickname )
+            return;
+
+        const options = {
+            keyboard: false,
+            show: true
+        };
+
+        $('#echoblaster-nickname-modal').modal(options);
+        $('#echoblaster-nickname-modal').on('hidden.bs.modal', function (e) {
+            let _nickname = $(this).find("#nickname-name").val();
+            if( _nickname )
+                $.cookie("echoblaster-nickname", btoa(_nickname));
+            checkNickname();
+        });
+    };
+
+    checkNickname();
+
+    document.getElementById("echoblaster-message-text").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode == 13){
+            let txt = $(this);
+            sendMessage(txt.val());
+            txt.val("");
+        }
+    });
 }
