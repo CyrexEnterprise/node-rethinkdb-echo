4336b9a4d284a6e0aeb90f7c5146b4a1b252cb18
Ricardo Malta <ricardo@cloudoki.com>
2016-12-30 09:51:05 +0000

    Implemented
    Better logging

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var winston = require('winston');
-var expressWinston = require('express-winston');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const
+    express         = require('express'),
+    path            = require('path'),
+    favicon         = require('serve-favicon'),
+    cookieParser    = require('cookie-parser'),
+    logger          = require('./lib/logger'),
+    bodyParser      = require('body-parser');
 
-var index = require('./lib/controllers/index');
-
-var app = express();
+const
+    index       = require('./lib/controllers/index');
+    messages    = require('./lib/controllers/messages'),
+    app         = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'lib/views'));
@@ -16,19 +17,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(expressWinston.logger({
-      transports: [
-        new winston.transports.Console({
-        //   json: true,
-          colorize: true
-        })
-      ],
-      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-      msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-      expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-      colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-      ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
-    }));
+app.use(logger.http);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
